948041f5addf49c00cfe88eafc16cc9bd6df5607
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-08 03:56:05 +0100

    created the button which voter can click on each candidate so a to select voter's choice

--- Ballot/src/components/Proposals.jsx
+++ Ballot/src/components/Proposals.jsx
@@ -5,7 +5,7 @@ import { Card, Flex, Box, Text  } from "@radix-ui/themes";
 const Proposals = (props) => {
     return (
         <Card style={{ maxWidth: 240 }}>
-            <Flex gap="3" align="center">
+            <Flex justify={"between"} gap="3" align="center">
                 <Box>
                     <Text as="div" size="2" weight="bold">
                         {props.name}
@@ -14,6 +14,7 @@ const Proposals = (props) => {
                         {props.voteCount}
                     </Text>
                 </Box>
+                <button  onClick={""}>Vote</button>
             </Flex>
         </Card>
     )
