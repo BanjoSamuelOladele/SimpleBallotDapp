f0eb2a6ded4c676db591e190389d6b074de93c8a
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-09 08:30:56 +0100

    inside the handlePermitVote function,checked if it is in the supported chain and also checked if the address is a valid valid address

--- Ballot/src/functions/permitVoter.js
+++ Ballot/src/functions/permitVoter.js
@@ -1,16 +1,37 @@
-import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+import { useWeb3ModalAccount, useWeb3ModalProvider } from "@web3modal/ethers/react";
+import { isSupportedChain } from "../utils/isSupportedChain";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import { isAddress } from "ethers";
+import { readAndChangeStateProvider } from "../constants/provider";
+import { getContract } from "../constants/contract";
 
 
 
 const handlePermitToVote = (address) => {
 
     const {chainId} = useWeb3ModalAccount();
+    const {walletProvider} = useWeb3ModalProvider();
 
-    return useEffect(() => {
-      
-        if(!isSupportedChain(chainId)) return;
-        
+
+    return useEffect(async () => {
+
+        if(!isSupportedChain(chainId)) return toast.error("not on the supported network");
+        if(!isAddress(address)) return toast.error("");
+
+        const readAndWrite = readAndChangeStateProvider(walletProvider);
+        const signer = readAndWrite.getSigner();
+
+        const contract = getContract(signer);
+
+        try{
+            const estimatedGas = await contract.giveRightToVote.estimateGas(address);
+        }catch(error){
+            
+        }
+
     }, [])
+    
     
 }
 export default handlePermitToVote;
--- Ballot/src/main.jsx
+++ Ballot/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme appearance='dark'>
         <App />
+        <ToastContainer />
      </Theme>
   </React.StrictMode>
 )
