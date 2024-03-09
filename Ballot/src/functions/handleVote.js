9e3f68148c4b24d09c06ae4dcd8fe4fdf944fd35
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-09 11:25:33 +0100

    added few attribute to the toast container

--- Ballot/src/functions/handleVote.js
+++ Ballot/src/functions/handleVote.js
@@ -3,17 +3,19 @@ import { isSupportedChain } from "../utils/isSupportedChain";
 import { toast } from "react-toastify";
 import { readAndChangeStateProvider } from "../constants/provider";
 import { getContract } from "../constants/contract";
+import handleSigner from "./handleSigner";
 
 
 const handleVote = async (id) => {
 
-    const {chainId} = useWeb3ModalAccount();
-    const {walletProvider} = useWeb3ModalProvider();
-    if(isSupportedChain(chainId)) return toast.error("");
-    const readAndChangeState = readAndChangeStateProvider(walletProvider);
-    const signer = await readAndChangeState.getSigner();
+    // const {chainId} = useWeb3ModalAccount();
+    // const {walletProvider} = useWeb3ModalProvider();
+    // if(isSupportedChain(chainId)) return toast.error("");
+    // const readAndChangeState = readAndChangeStateProvider(walletProvider);
+    // const signer = await readAndChangeState.getSigner();
 
-    const contract = getContract(signer);
+    
+    const contract = getContract(handleSigner());
     
     try {
         const transaction = await contract.vote(id);
--- Ballot/src/main.jsx
+++ Ballot/src/main.jsx
@@ -11,7 +11,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme appearance='dark'>
         <App />
-        <ToastContainer />
+        <ToastContainer 
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition: Bounce
+        />
      </Theme>
   </React.StrictMode>
 )
